c9f1852041a90ec6ca947cca587862d9562d2f22
Maxim Andrievskiy <maxim.andrievskiy2003@gmail.com>
2022-04-02 08:57:22 +0000

    add CountWords function

--- src/main.c
+++ src/main.c
@@ -1,8 +1,33 @@
 /* hello.c */
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#pragma warning(disable: 4996)
 
-void main (void)
-{
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
+int CountWords (const char* str) {
+	int wordsNum = 0;
+	int len = strlen(str);
+	int j = 0;
+	for (int i = 0; i < len - 1; ++i) {
+		if (!strncmp(&str[i], ", ", 2) && i != j) {
+			++wordsNum;
+			j = i + 2;
+			++i;
+		}		
+	}
+	if (strcmp(&str[j], "\0")) {
+		++wordsNum;
+	}
+	return wordsNum;
+}
+
+int main(int argc, char* argv[]) {
+	const int maxStrLength = 100;
+	const int maxWordLength = 20;
+	char* str = (char*)malloc(maxStrLength);
+	strcpy(str, argv[1]);
+	int wordsNum = CountWords(str);
+	int len = strlen(str);
+	return 0;
+
 }
