a2fe33626ea0022ddfb2f13e48d64f1d64546b91
Maxim Andrievskiy <maxim.andrievskiy2003@gmail.com>
2022-04-08 16:37:01 +0000

    add DivideIntoWords function

--- src/main.c
+++ src/main.c
@@ -21,13 +21,39 @@ int CountWords (const char* str) {
 	return wordsNum;
 }
 
+void DivideIntoWords(const char* str, int wordsNum, char*** words) {
+	int len = strlen(str);
+	int j = 0, k = 0;
+	for (int i = 0; i < len - 1; ++i) {
+		if (!strncmp(&str[i], ", ", 2) && i != j) {
+			strncpy((*words)[k], &str[j], i - j);
+			j = i + 2;
+			++i;
+			++k;
+		}
+	}
+	if (strcmp(&str[j], "\0")) {
+		strncpy((*words)[k], &str[j], len - j);
+	}
+
+}
+
 int main(int argc, char* argv[]) {
 	const int maxStrLength = 100;
 	const int maxWordLength = 20;
 	char* str = (char*)malloc(maxStrLength);
 	strcpy(str, argv[1]);
+	printf("%s\n", str);
 	int wordsNum = CountWords(str);
 	int len = strlen(str);
+	printf("%d\n", wordsNum);	
+	char** words = (char**)malloc(wordsNum * sizeof(char*));
+	for (int i = 0; i < wordsNum; ++i) {
+		words[i] = (char*)malloc(maxWordLength * sizeof(char));
+	}
+	DivideIntoWords(str, wordsNum, &words);
+	for (int i = 0; i < wordsNum; ++i) {
+		printf("Words[%d] = %s; Len = %d\n", (int)i, words[i], (int)strlen(words[i]));
+	}
 	return 0;
-
 }
